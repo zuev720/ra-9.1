bc854fdcfefe94ce676893fd9187e09824aa37aa
zuev720 <zuev720mail.ru>
2021-09-08 20:40:12 +0300

    Поправил дефолтную страницу

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -4,7 +4,7 @@ import {NavLink} from 'react-router-dom';
 export function Menu() {
     return (
         <nav className="menu">
-            <NavLink exact activeClassName={'menu__item-active'} to={'/'} className={'menu__item'}>Главная</NavLink>
+            <NavLink exact to={'/'} activeClassName={'menu__item-active'} className={'menu__item'}>Главная</NavLink>
             <NavLink activeClassName={'menu__item-active'}  to={'/drift'} className={'menu__item'}>Дрифт-такси</NavLink>
             <NavLink activeClassName={'menu__item-active'} to={'/timeattack'} className={'menu__item'}>Time Attack</NavLink>
             <NavLink activeClassName={'menu__item-active'} to={'/forza'} className={'menu__item'}>Forza Karting</NavLink>
