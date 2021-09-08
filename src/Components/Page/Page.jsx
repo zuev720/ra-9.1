c59ffe067d7c0b036af9bcf0247bc55851e0f6cd
zuev720 <zuev720mail.ru>
2021-09-08 20:19:30 +0300

    Пытаюсь решить проблему стартовой страницы

--- src/Components/Page/Page.jsx
+++ src/Components/Page/Page.jsx
@@ -4,7 +4,7 @@ import {arrayObjectPage} from '../Base/arrayObjectPage';
 
 export function Page (props) {
     const match = props.match.path.substr(1);
-    const matchObject = arrayObjectPage.find((elem) => elem.name === match);
+    const matchObject = arrayObjectPage.find((elem) => elem.name == match);
     const Description = () => matchObject.description.map((elem, index) => <Text key={index} description={elem} />);
 
     return(
