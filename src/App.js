14ee0fe122d50d5f19ef333bcfbfced1a52e78d4
zuev720 <zuev720mail.ru>
2021-09-20 13:28:32 +0300

    rename basename in Router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import {BrowserRouter as Router, Redirect, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import {Menu} from './Components/Menu/Menu';
 import {Page} from './Components/Page/Page';
 import './App.css';
 
 export default function App() {
     return (
-        <Router basename={'https://zuev720.github.io/ra-9.1'}>
+        <Router basename={'/ra-9.1'}>
             <div>
                 <Menu/>
                 <div className="page">
-                    <Route path={'/ra-9.1'} exact={true} component={Page}/>
+                    <Route path={'/'} exact={true} component={Page}/>
                     <Route path={'/drift'} component={Page}/>
                     <Route path={'/timeattack'} component={Page}/>
                     <Route path={'/forza'} component={Page}/>
-                    <Redirect to={'/ra-9.1'} />
                 </div>
             </div>
         </Router>
